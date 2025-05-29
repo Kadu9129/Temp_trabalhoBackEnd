5ab81a1738e4f6d1ecd6f5399441cff3ce2ac3b3
Kadu9129 <Carlos9129@outlook.com.br>
2025-05-29 17:49:45 -0300

    Att

--- e_shop/produtos/urls.py
+++ e_shop/produtos/urls.py
@@ -1,12 +1,28 @@
 
+# from django.urls import path
+# from .views import (
+#     ProdutoListView, ProdutoCreateView,
+#     ProdutoUpdateView, ProdutoDeleteView
+# )
+
+# urlpatterns = [
+#     path('', ProdutoListView.as_view(), name='listar_produtos'),
+#     path('adicionar/', ProdutoCreateView.as_view(), name='adicionar_produto'),
+#     path('editar/<int:pk>/', ProdutoUpdateView.as_view(), name='editar_produto'),
+#     path('remover/<int:pk>/', ProdutoDeleteView.as_view(), name='remover_produto'),
+# ]
+
+
 from django.urls import path
 from .views import (
+    menu,
     ProdutoListView, ProdutoCreateView,
     ProdutoUpdateView, ProdutoDeleteView
 )
 
 urlpatterns = [
-    path('', ProdutoListView.as_view(), name='listar_produtos'),
+    path('', menu, name='menu'), 
+    path('produtos/', ProdutoListView.as_view(), name='listar_produtos'),
     path('adicionar/', ProdutoCreateView.as_view(), name='adicionar_produto'),
     path('editar/<int:pk>/', ProdutoUpdateView.as_view(), name='editar_produto'),
     path('remover/<int:pk>/', ProdutoDeleteView.as_view(), name='remover_produto'),
--- e_shop/produtos/views.py
+++ e_shop/produtos/views.py
@@ -1,23 +1,25 @@
+from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Produto
 from .forms import ProdutoForm
 
 
+def menu(request):
+    produtos = Produto.objects.all()
+    return render(request, 'produtos/menu.html', {'produtos': produtos})
+
 
 class ProdutoListView(ListView):
     model = Produto
-    template_name = 'produtos/lista.html'
+    template_name = 'produtos/cadastro.html'
     context_object_name = 'produtos'
-
-
 
 class ProdutoCreateView(CreateView):
     model = Produto
     form_class = ProdutoForm
     template_name = 'produtos/form.html'
     success_url = reverse_lazy('listar_produtos')
-
 
 
 class ProdutoUpdateView(UpdateView):
@@ -29,5 +31,5 @@ class ProdutoUpdateView(UpdateView):
 
 class ProdutoDeleteView(DeleteView):
     model = Produto
-    template_name = 'produtos/confirm_delete.html'  
+    template_name = 'produtos/confirm_delete.html' 
     success_url = reverse_lazy('listar_produtos')
